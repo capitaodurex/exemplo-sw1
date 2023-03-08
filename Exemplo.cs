90e55b14115594f68cd857920f0c39647de0be0c
capitaodurex <100379738+capitaodurex@users.noreply.github.com>
2023-03-08 16:54:44 -0300

    Add files via upload

---
+++ Exemplo.cs
@@ -0,0 +1,28 @@
+namespace exercicio;
+class Exemplo
+{
+    public string nome="";
+
+    public int idade=0;
+
+    public bool vacinado=false;
+
+public void mostramsg(){
+    Console.WriteLine("poggers");
+
+    
+}
+public void pegaNome(string texto){
+        Console.WriteLine("Daleee," + texto);
+    }
+
+    public string mensagem(){
+        return "Devolvendo um texto";
+    }
+
+    public int somar(int a, int b){
+        return a+b;
+    }
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace exercicio;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine("Hello, World!");
+        //Console.ReadKey();
+
+        Exemplo exe96 = new Exemplo();
+
+        exe96.mostramsg();
+
+        exe96.pegaNome("ANDERSON");
+
+       Console.WriteLine(exe96.mensagem());
+
+       Console.WriteLine("O resultado da soma é: " + exe96.somar(10,20));
+
+    }
+}
